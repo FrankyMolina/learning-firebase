92fa58d1d9b46228cefd085cbb290be42c183251
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-10-28 18:30:38 +0100

    cleaning files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 //we wrap everything in our authprovider component, and get our useAuth funct to know if we have a currentUser
 import AuthProvider from './api/auth/AuthProvider';
@@ -18,8 +17,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
